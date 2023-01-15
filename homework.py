90b6856e916b481151ae3f640aa125bc2dd210a5
Алексеев Андрей <wembeded@gmail.com>
2023-01-15 22:09:25 +0300

    Some code repair

--- homework.py
+++ homework.py
@@ -166,7 +166,7 @@ def main(training: Training) -> None:
 
 
 if __name__ == '__main__':
-    packages: None = [
+    packages = [
         ('SWM', [720, 1, 80, 25, 40]),
         ('RUN', [15000, 1, 75]),
         ('WLK', [9000, 1, 75, 180]),
